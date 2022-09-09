9f7e18aa8fef9bb3dc923bf4600b1b6f5f7400d6
Eloisa1801 <99847182+Eloisa1801@users.noreply.github.com>
2022-09-08 20:19:49 -0400

    Add files via upload

---
+++ eloisa.sql
@@ -0,0 +1,13 @@
+-- exercicio 01
+SELECT * FROM Produto;
+
+-- exercicio 02
+
+SELECT id, nome, quantidade_minima, unidade_medida 
+FROM Produto
+ORDER BY quantidade_minima DESC
+
+-- exercicio 03
+
+-- select nome, id FROM produto;
+-- SELECT * FROM Produto WHERE NOT nome = 'pão' OR nome ='tomate';
