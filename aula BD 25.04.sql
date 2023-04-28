8a67e57b331823177c468d796b74497d54631d1a
Eloisa1801 <99847182+Eloisa1801@users.noreply.github.com>
2023-04-27 22:38:41 -0400

    Update aula BD 25.04.sql

--- aula BD 25.04.sql
+++ aula BD 25.04.sql
@@ -25,4 +25,15 @@ SELECT * FROM cidade2
 alter table fake2
 add column cidade int references cidade2(codigo);
 
+select number, givenname, city, state, statefull, cidade from fake2;
+update fake2 set cidade = cidade2.codigo
+from cidade2
+where city = cidade2.nome and state = sigla_estado;
+
 select number, givenname, city, state, statefull, cidade from fake2
+order by number;
+select * from cidade2 where codigo in (32, 329, 105);
+
+ALTER TABLE Fake2 DROP COLUMN city;
+ALTER TABLE Fake2 DROP COLUMN state;
+ALTER TABLE Fake2 DROP COLUMN statefull;
