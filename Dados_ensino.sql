026524cc39a259dcbbfc055e130e7a0dcf70a0ed
Eloisa1801 <99847182+Eloisa1801@users.noreply.github.com>
2023-05-23 19:23:28 -0400

    Update Dados_ensino.sql

--- Dados_ensino.sql
+++ Dados_ensino.sql
@@ -18,4 +18,58 @@ ALTER TABLE Dados_ensino drop column NO_REGIAO;
 
 /*------------------------*/
 
+create table Regiao
+(
+   codigo int primary key,
+   nome varchar(30) not null
+);
+
+INSERT INTO Regiao 
+SELECT DISTINCT CO_REGIAO, NO_REGIAO  
+FROM Dados_Ensino
+order by 1;
+
+select * from Regiao;
+
+ALTER TABLE Dados_ensino drop column NO_REGIAO;
+
+/*------------------------*/
+
+CREATE TABLE Uf
+(
+   codigo int primary key,
+   nome varchar(100),
+   sigla varchar(2)
+);
+
+INSERT INTO Uf
+SELECT DISTINCT CO_UF, NO_UF, SG_UF, 
+FROM Dados_ensino
+ORDER BY 1;
+
+select * from Uf;
+
+ALTER TABLE Dados_ensino drop column NO_UF;
+ALTER TABLE Dados_ensino drop column SG_UF;
+
+/*------------------------*/
+
+CREATE TABLE Municipio
+(
+   codigo varchar(20) primary key,
+   nome varchar(100)
+);
+
+INSERT INTO Municipio
+SELECT DISTINCT CO_MUNICIPIO, NO_MUNICIPIO
+FROM Dados_ensino
+ORDER BY 1;
+
+select * from Municipio;
+
+ALTER TABLE Dados_ensino drop column NO_MUNICIPIO;
+
+/*------------------------*/
+
+
 
